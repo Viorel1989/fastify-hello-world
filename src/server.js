ee159a931f45fe62c19803309f48b30ca7b74f10
Viorel <viorel_popa89@yahoo.com>
2024-08-13 09:38:28 +0300

    chore(server): update server port usage

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const server = require('./app')({
   },
 })
 
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 server.listen({ port: port, host: '0.0.0.0' }, (err, address) => {
   if (err) {
